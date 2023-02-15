55684b7991be613457785e03f1640902dedc1127
Muhammad Bilal Shaikh <mbs.techy@gmail.com>
2023-02-15 21:38:36 +0800

    added search

--- django_azure/settings.py
+++ django_azure/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@ec@f-9lc=&kaujc4fnbknyyjxjo7tpivr($x)28iqaejx+uy)"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
